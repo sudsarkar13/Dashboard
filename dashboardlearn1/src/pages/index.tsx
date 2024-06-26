60cb8e69a12fb2402a954e40cddd11e98bc5f831
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-26 22:59:32 +0530

    Update index.tsx

--- dashboardlearn1/src/pages/index.tsx
+++ dashboardlearn1/src/pages/index.tsx
@@ -1,14 +1,17 @@
-import Image from "next/image";
 import { ubuntuMono } from "@/types/font/Font";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <main className={`min-h-screen ${ubuntuMono.className}`}>
-      <div className={``}>
-        <h1 className="text-2xl text-center font-[600]">
-          Welcome to Dashboard
-        </h1>
-      </div>
-    </main>
-  );
+	return (
+		<main className={`min-h-screen ${ubuntuMono.className}`}>
+			<Head>
+				<title>Home | Dashboard</title>
+			</Head>
+			<div className={``}>
+				<h1 className='text-2xl text-center font-[600]'>
+					Welcome to Dashboard
+				</h1>
+			</div>
+		</main>
+	);
 }
