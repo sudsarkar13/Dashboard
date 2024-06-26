72617464ac89955028c16abae010902cb8787b67
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-26 21:45:24 +0530

    Update Font.ts

--- dashboardlearn1/src/types/font/Font.ts
+++ dashboardlearn1/src/types/font/Font.ts
@@ -3,18 +3,20 @@ import { Ubuntu, Ubuntu_Mono, Ubuntu_Condensed } from "next/font/google";
 export const ubuntu = Ubuntu({
   subsets: ["cyrillic", "cyrillic-ext", "greek", "greek-ext", "latin", "latin-ext"],
   style: ["normal"],
+  variable: "--font-sans",
   weight: ["300", "400", "500", "700"],
 });
 
 export const ubuntuMono = Ubuntu_Mono({
   subsets: ["cyrillic", "cyrillic-ext", "greek", "greek-ext", "latin", "latin-ext"],
   style: ["normal"],
-  variable: "--font-ubuntu-mono",
+  variable: "--font-sans",
   weight: ["400", "700"],
 });
 
 export const ubuntuCondensed = Ubuntu_Condensed({
   subsets: ["cyrillic", "cyrillic-ext", "greek", "greek-ext", "latin", "latin-ext"],
   style: ["normal"],
+  variable: "--font-sans",
   weight: ["400"],
 });
