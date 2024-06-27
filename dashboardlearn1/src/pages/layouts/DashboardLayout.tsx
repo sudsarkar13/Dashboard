01128974a85c4d7de43c60808f0d82ab6ff478de
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-27 15:55:36 +0530

    Create DashboardLayout.tsx

---
+++ dashboardlearn1/src/pages/layouts/DashboardLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardLayout: React.FC = () => {
+  return (
+    <main>
+      <div>DashboardLayout</div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
