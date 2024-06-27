f05908823833b30b60159f67949658e6e0a053fa
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-27 06:15:39 +0530

    Prettier code formatted

--- dashboardlearn1/src/pages/login/Login.tsx
+++ dashboardlearn1/src/pages/login/Login.tsx
@@ -2,12 +2,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -15,49 +15,49 @@ import Head from "next/head";
 import Link from "next/link";
 
 const Login: React.FC = () => {
-	return (
-		<main>
-			<Head>
-				<title>Dashboard | Login</title>
-			</Head>
-			<div className='flex min-h-screen items-center justify-center'>
-				<Card className='w-full max-w-sm'>
-					<CardHeader>
-						<CardTitle className='text-2xl'>Login</CardTitle>
-						<CardDescription>
-							Enter your email below to login to your account.
-						</CardDescription>
-					</CardHeader>
-					<CardContent className='grid gap-4'>
-						<div className='grid gap-2'>
-							<Label htmlFor='email'>Email</Label>
-							<Input
-								id='email'
-								type='email'
-								placeholder='m@example.com'
-								required
-							/>
-						</div>
-						<div className='grid gap-2'>
-							<Label htmlFor='password'>Password</Label>
-							<Input id='password' type='password' required />
-						</div>
-					</CardContent>
-					<CardFooter>
-						<div className="w-full">
-							<Button className='w-full'>Sign in</Button>
-							<div className='mt-4 text-center text-sm'>
-								Don't have an account?{" "}
-								<Link href='/register' className='underline'>
-									Register here
-								</Link>
-							</div>
-						</div>
-					</CardFooter>
-				</Card>
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <Head>
+        <title>Dashboard | Login</title>
+      </Head>
+      <div className="flex min-h-screen items-center justify-center">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="w-full">
+              <Button className="w-full">Sign in</Button>
+              <div className="mt-4 text-center text-sm">
+                Don't have an account?{" "}
+                <Link href="/register" className="underline">
+                  Register here
+                </Link>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
