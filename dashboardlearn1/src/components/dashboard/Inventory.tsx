93b84a0160972cd2ea5dab06e820be8cad77be1f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-29 00:26:13 +0530

    Update Inventory.tsx

--- dashboardlearn1/src/components/dashboard/Inventory.tsx
+++ dashboardlearn1/src/components/dashboard/Inventory.tsx
@@ -5,7 +5,7 @@ export default function Inventory() {
 	return (
 		<main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-4 h-screen '>
 			<div className='flex items-center'>
-				<h1 className='text-lg font-semibold md:text-2xl'>Inventory</h1>
+				<h1 className='text-lg font-semibold md:text-2xl'>Dashboard</h1>
 			</div>
 			<div
 				className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm overflow-hidden'
@@ -13,12 +13,12 @@ export default function Inventory() {
 			>
 				<div className='flex flex-col items-center gap-1 text-center'>
 					<h3 className='text-2xl font-bold tracking-tight'>
-						You have no products
+						You have no books
 					</h3>
 					<p className='text-sm text-muted-foreground'>
-						You can start selling as soon as you add a product.
+						You can start selling as soon as you add a book.
 					</p>
-					<Button className='mt-4'>Add Product</Button>
+					<Button className='mt-4'>Add Book</Button>
 				</div>
 			</div>
 		</main>
