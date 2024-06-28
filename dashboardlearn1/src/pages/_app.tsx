221b1a902a9802788b9f232fb1fc58c1105ad811
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-29 05:08:07 +0530

    Added axios and tanstack query

--- dashboardlearn1/src/pages/_app.tsx
+++ dashboardlearn1/src/pages/_app.tsx
@@ -1,17 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/dashboard/Layout";
 import AuthLayout from "@/components/auth/AuthLayout";
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps, router }: AppProps) {
-	const isAuthRoute = router.pathname.startsWith("/auth");
-	return isAuthRoute ? (
-		<AuthLayout>
-			<Component {...pageProps} />
-		</AuthLayout>
-	) : (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  const isAuthRoute = router.pathname.startsWith("/auth");
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {isAuthRoute ? (
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </QueryClientProvider>
+  );
 }
