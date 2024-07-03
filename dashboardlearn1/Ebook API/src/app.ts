49271be0428b3e8bcf8d364dc189ff98d134d1d6
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-03 20:50:31 +0530

    Added register user validation

--- dashboardlearn1/Ebook API/src/app.ts
+++ dashboardlearn1/Ebook API/src/app.ts
@@ -4,6 +4,7 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json());
 
 // Routes
 // Http methods: GET, POST, PUT, PATCH, DELETE etc.
@@ -16,7 +17,7 @@ app.get("/", (req, res, next) => {
 	res.json({ message: "Welcome to Ebook API's" });
 });
 
-app.use("/api/users",userRouter);
+app.use("/api/users", userRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
