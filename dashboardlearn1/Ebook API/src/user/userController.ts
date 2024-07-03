fca3a512d15b3ad51a4c4dc751be389d70a9f9db
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-03 21:18:50 +0530

    Add user model

--- dashboardlearn1/Ebook API/src/user/userController.ts
+++ dashboardlearn1/Ebook API/src/user/userController.ts
@@ -1,16 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, email, password } = req.body;
-  // validation
-  if(!name || !email || !password) {
-    const error = createHttpError(400, "All fields are required");
+	const { name, email, password } = req.body;
+	// validation
+	if (!name || !email || !password) {
+		const error = createHttpError(400, "All fields are required");
+		return next(error);
+	}
+	// database call
+	const user = await userModel.findOne({ email });
+  if(user) {
+    const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
-  // process logic
-  // response
+	// process logic
+	// response
 	res.json({ message: "User Created" });
 };
 
