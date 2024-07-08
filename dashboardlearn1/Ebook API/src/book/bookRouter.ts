353fbd488675d411d12685329846d834cdd74a50
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-08 11:59:33 +0530

    setup multer

--- dashboardlearn1/Ebook API/src/book/bookRouter.ts
+++ dashboardlearn1/Ebook API/src/book/bookRouter.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path";
 
 const bookRouter = express.Router();
+const upload = multer({
+	dest: path.resolve(__dirname, "../../public/temp/data/uploads"),
+	limits: { fileSize: 5e7 }, // 50 MB
+});
 // Routes
 // /api/books
-bookRouter.post("/", createBook);
+bookRouter.post(
+	"/",
+	upload.fields([
+		{ name: "coverImage", maxCount: 1 },
+		{ name: "file", maxCount: 1 },
+	]),
+	createBook
+);
 export default bookRouter;
