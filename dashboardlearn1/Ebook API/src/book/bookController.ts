7b214b15a8deec1f0d26844618b0b6cfdd476be4
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-09 04:16:09 +0530

    Update environment example

--- dashboardlearn1/Ebook API/src/book/bookController.ts
+++ dashboardlearn1/Ebook API/src/book/bookController.ts
@@ -2,7 +2,8 @@
 import { Request, Response, NextFunction } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  const {} = req.body;
+  // const {} = req.body;
+  console.log("files", req.files);
 	res.json({});
 };
 
---
+++ dashboardlearn1/Ebook API/src/config/cloudinary.ts
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: "dxm3bgmzh",
+  api_key: "553389325576593",
+  api_secret: "ODFFqLAOgLq5KVTTy453M-e503E",
+});
