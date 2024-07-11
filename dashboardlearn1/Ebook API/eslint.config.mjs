57c6938db9659d612e24c9dd505b3b6ea04304a0
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-07-11 21:04:35 +0530

    Update eslint.config.mjs

--- dashboardlearn1/Ebook API/eslint.config.mjs
+++ dashboardlearn1/Ebook API/eslint.config.mjs
@@ -7,5 +7,9 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {rules: {'@typescript-eslint/no-unused-vars': 'off',}}
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
